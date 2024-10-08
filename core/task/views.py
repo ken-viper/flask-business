bdecb0be6dc6136e2d48fec8e11ecad9f64c340b
botbenz82 <botbenz111@gmail.com>
2024-10-08 10:30:55 +0900

    chore:this commit should improve the task view so that the only user that login can see the own task

--- core/task/views.py
+++ core/task/views.py
@@ -1,4 +1,7 @@
-from flask import render_template, redirect, url_for, request
+
+
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_required, current_user #new line
 from .models import Category
 from ..models import Todo
 from . import task
@@ -7,15 +10,17 @@ from .. import db
 from datetime import datetime
 
 @task.route('/create-task', methods=['GET', 'POST'])
+@login_required #new line
 def tasks():
     check= None
-    todo= Todo.query.all()
+    user = current_user #new line
+    todo= Todo.query.filter_by(author=user) #new line
     date= datetime.now()
     now= date.strftime("%Y-%m-%d")
 
     form= TaskForm()
     form.category.choices =[(category.id, category.name) for category in Category.query.all()]
-    
+
     if request.method == "POST":
         if request.form.get('taskDelete') is not None:
             deleteTask = request.form.get('checkedbox')
@@ -30,10 +35,13 @@ def tasks():
         elif form.validate_on_submit():
             selected= form.category.data
             category= Category.query.get(selected)
-            todo = Todo(title=form.title.data, date=form.date.data, time= form.time.data, category= category.name)
+            todo = Todo(title=form.title.data, date=form.date.data, time= form.time.data, category= category.name, author=user) #new line
             db.session.add(todo)
             db.session.commit()
+            flash('Congratulations, you just added a new note')
             return redirect(url_for('task.tasks'))
 
     return render_template('task/tasks.html', title='Create Tasks', form=form, todo=todo, DateNow=now, check=check)
+
+
 
